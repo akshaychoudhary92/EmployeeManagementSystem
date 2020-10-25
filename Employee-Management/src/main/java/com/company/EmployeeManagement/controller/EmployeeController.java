0389d3fd3820ecc6c4f1d7209ffa801e22662f8e
Akshay Choudhary <ak92choudhary@gmail.com>
2020-10-24 22:12:22 -0400

    made create employee component and view component

--- Employee-Management/src/main/java/com/company/EmployeeManagement/controller/EmployeeController.java
+++ Employee-Management/src/main/java/com/company/EmployeeManagement/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.company.EmployeeManagement.controller;
 import com.company.EmployeeManagement.model.Employee;
 import com.company.EmployeeManagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
@@ -21,5 +18,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    // create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -9,14 +10,22 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
+      <Router>
+        <div className = "container">
       <HeaderComponent/>
 
     <div className="container">
-      <ListEmployeeComponent/>
-
+    <Switch>
+    <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+    <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+    <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
+      </Switch>
     </div>
     <FooterComponent/>
     </div>
+    </Router>
+    </div>
+
   );
 }
 
